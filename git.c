cda5b3657cae1d39579e484ee61ed0bb3ef59587
Orion <852192419@qq.com>
2021-10-24 00:15:19 +0800

    first commit

--- git.c
+++ git.c
@@ -1,13 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-10-23 19:57:01
- * @LastEditTime: 2021-10-23 22:03:34
+ * @LastEditTime: 2021-10-23 23:11:08
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \git\git.c
  */
 #include "stdio.h"
-
+/**
+ * main
+ */
 int main(void)
 {
     printf("helloworld");
