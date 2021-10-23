34d25fbf7dd493eadbb88125c9b5ead9bfef4f42
Orion <852192419@qq.com>
2021-10-23 21:06:14 +0800

    master

---
+++ git.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-23 19:57:01
+ * @LastEditTime: 2021-10-23 20:41:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \git\git.c
+ */
+#include "stdio.h"
+
+void main(void)
+{
+    printf("helloworld");
+}
